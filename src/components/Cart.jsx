cb7008911f3826e6466ae14ec7369ac0bd40e5b1
henglysok <sokhenglystudent@gmail.com>
2025-05-27 11:06:54 +0700

    just fix the button allupdate

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -53,7 +53,7 @@ export function CartList() {
         <div className=" relative grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-[33px] bg-black pt-20 px-5">
             <div className="absolute top-0 left- ps-5 pt-2 flex justify-between items-center w-full">
                 <span className="text-text-100 0 text-[28px] ">Recently Update</span>
-                <span className="flex items-center justify-between  bg-secondary-100 rounded-s-[5px] transition-all duration-300 ease-in-out hover:rounded-s-[20px] hover:px-[15px]">
+                <span className="flex items-center justify-between pe-5 bg-secondary-100 rounded-s-[5px] transition-all duration-300 ease-in-out hover:rounded-s-[20px] hover:px-[15px] hover:pe-10">
                     <button className="py-1 px-4 text-[18px] hover:">All Update</button>
                     <svg xmlns="http://www.w3.org/2000/svg"
                         width="16" height="16"
