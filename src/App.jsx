b61a479f11dd0e34bf5812ffb67ea5d236613419
henglysok <sokhenglystudent@gmail.com>
2025-06-01 16:38:02 +0700

    1/6/24

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,35 @@ import SocialMediaBanner from './components/SocialMediaBanner'
 
 function App() {
 
+  // <div className='grid grid-cols-1 md:grid-cols-3 justify-center gap-[26px]'>
+  //         <div className="col-span-2">
+  //           <CartList />
+  //         </div>
+  //         <div className='flex flex-col gap-3'>
+  //           {/*socal media banner*/}
+  //           <SocialMediaBanner />
+  //           <HottestCartList />
+  //         </div>
+  //       </div>
+
 
   return (
-    <section className='w-full'>
+    <section className='w-full  '>
       <NavBar />
       <Banner />
-      <div className='w-full  md:w-[90%] mx-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-3 justify-between gap-[26px] mx-auto'>
-          <div className="col-span-2">
+      <div className='w-[90%] mx-auto'>
+
+        <div className='flex md:justify-center lg:justify-center md:gap-3 w-full'>
+          <div className='w-ful'>
             <CartList />
           </div>
-          <div className='flex flex-col gap-3'>
-            {/*socal media banner*/}
+          <div className='max-w-[350px] flex flex-col gap-5'>
             <SocialMediaBanner />
             <HottestCartList />
           </div>
 
         </div>
+
       </div>
     </section>
   )
--- src/components/HottestCart.jsx
+++ src/components/HottestCart.jsx
@@ -91,7 +91,7 @@ export function HottestCartList() {
                 </div>
 
                 {/* Header */}
-                <div className="w-full flex justify-between items-center gap-3 bg-primary-100 ps-5 py-5">
+                <div className="w-min-[271px] w-full flex justify-between items-center gap-3 bg-primary-100 ps-5 py-5">
                     <div className="flex items-center gap-3">
                         <svg xmlns="http://www.w3.org/2000/svg"
                             width="24" height="24" fill="currentColor"
@@ -101,13 +101,13 @@ export function HottestCartList() {
                         >
                             <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16m0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15" />
                         </svg>
-                        <h3 className="text-text-100 text-[24px] font-bold">Hottest</h3>
+                        <h3 className="text-text-100 lg:text-[24px] font-bold">Hottest</h3>
                     </div>
 
                     <span className="flex items-center gap-3 justify-between px-5 py-2 bg-secondary-100 rounded-s-[5px] cursor-pointer
                                      transition-all duration-300 ease-in-out hover:rounded-s-[20px] hover:px-[20px] hover:pe-10"
                     >
-                        <button className="text-sm font-semibold">ALL VIEW</button>
+                        <button className="text-sm font-semibold text-nowrap">ALL VIEW</button>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="16" height="16" fill="currentColor"
