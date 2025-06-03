dc733ea953ee74841aa5a20f2dc61678e724af06
henglysok <sokhenglystudent@gmail.com>
2025-06-03 20:48:01 +0700

    change pc

--- src/Pages/Feature.jsx
+++ src/Pages/Feature.jsx
@@ -1,24 +1,13 @@
+import Carousel from "../components/Carousel"
 import { FeatureCard } from "../components/Feature/FeatureCard"
 
 
 function Feature() {
     return (
-        <div className='h-full w-full flex items-center justify-center bg-black flex-col gap-20 '>
+        <div className='h-full w-full flex items-center justify-center bg-black flex-col gap-20 overflow-hidden'>
             <h1 className='text-5xl text-white'>From Feature Page</h1>
-            <div className="flex gap-5  ">
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-                <FeatureCard />
-            </div>
-
+            <FeatureCard />
+            <Carousel />
         </div>
     )
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,15 @@
+import { FeatureCard } from "./Feature/FeatureCard";
+
+
+
+
+function Carousel() {
+
+    return (
+        <div className="flex gap-5 w-[600px] overflow-scroll">
+          <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Carousel
