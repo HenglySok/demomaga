2cc3781c9863cba549b8225afc273dabeafab14e
henglysok <sokhenglystudent@gmail.com>
2025-06-17 12:27:32 +0700

    back up plane

--- src/Pages/Feature.jsx
+++ src/Pages/Feature.jsx
@@ -6,40 +6,42 @@ export default function Feature() {
   return (
     <>
       <Banner />
-      <div className="relative w-full bg-gray-900">
-        <div className=" bg-black my-3 p-3">
-          <h3 className="text-xl text-text-100">WEEKLY SHONEN JUM</h3>
+      <div className='max-w-screen-2xl mx-auto'>
+        <div className="relative w-full bg-gray-900">
+          <div className=" bg-black my-3 p-3">
+            <h3 className="text-xl text-text-100">WEEKLY SHONEN JUM</h3>
+          </div>
+          <div className="flex  items-center scrollbar-x-hide overflow-x-auto scrollbar-hide h-fit gap-6 px-4">
+            {featurecard.map((item) => (
+              <div key={item.id} className="flex-shrink-0 w-[152px] h-[228px] rounded-xl overflow-hidden shadow-lg ">
+                <FeatureCard image={item.image} />
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex  items-center scrollbar-x-hide overflow-x-auto scrollbar-hide h-fit gap-6 px-4">
-          {featurecard.map((item) => (
-            <div key={item.id} className="flex-shrink-0 w-[152px] h-[228px] rounded-xl overflow-hidden shadow-lg ">
-              <FeatureCard image={item.image} />
-            </div>
-          ))}
+        <div className="relative w-full bg-gray-900">
+          <div className=" bg-black my-3 p-3">
+            <h3 className="text-xl text-text-100">WEEKLY SHONEN JUM</h3>
+          </div>
+          <div className="flex  items-center scrollbar-x-hide overflow-x-auto scrollbar-hide h-fit gap-6 px-4">
+            {featurecard.map((item) => (
+              <div key={item.id} className="flex-shrink-0 w-[152px] h-[228px] rounded-xl overflow-hidden shadow-lg ">
+                <FeatureCard image={item.image} />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="relative w-full bg-gray-900">
-        <div className=" bg-black my-3 p-3">
-          <h3 className="text-xl text-text-100">WEEKLY SHONEN JUM</h3>
-        </div>
-        <div className="flex  items-center scrollbar-x-hide overflow-x-auto scrollbar-hide h-fit gap-6 px-4">
-          {featurecard.map((item) => (
-            <div key={item.id} className="flex-shrink-0 w-[152px] h-[228px] rounded-xl overflow-hidden shadow-lg ">
-              <FeatureCard image={item.image} />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="relative w-full bg-gray-900">
-        <div className=" bg-black my-3 p-3">
-          <h3 className="text-xl text-text-100">WEEKLY SHONEN JUM</h3>
-        </div>
-        <div className="flex  items-center scrollbar-x-hide overflow-x-auto scrollbar-hide h-fit gap-6 px-4">
-          {featurecard.map((item) => (
-            <div key={item.id} className="flex-shrink-0 w-[152px] h-[228px] rounded-xl overflow-hidden shadow-lg ">
-              <FeatureCard image={item.image} />
-            </div>
-          ))}
+        <div className="relative w-full bg-gray-900">
+          <div className=" bg-black my-3 p-3">
+            <h3 className="text-xl text-text-100">WEEKLY SHONEN JUM</h3>
+          </div>
+          <div className="flex  items-center scrollbar-x-hide overflow-x-auto scrollbar-hide h-fit gap-6 px-4">
+            {featurecard.map((item) => (
+              <div key={item.id} className="flex-shrink-0 w-[152px] h-[228px] rounded-xl overflow-hidden shadow-lg ">
+                <FeatureCard image={item.image} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
